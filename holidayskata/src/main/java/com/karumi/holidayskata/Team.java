33045ac2bcc2514a87ba1403939360094d07bb68
Jorge J Barroso <jorge@karumi.com>
2015-12-30 00:35:50 +0100

    [Green] check availability of team member for a date.

--- holidayskata/src/main/java/com/karumi/holidayskata/Team.java
+++ holidayskata/src/main/java/com/karumi/holidayskata/Team.java
@@ -1,14 +1,23 @@
 package com.karumi.holidayskata;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class Team {
+    private List<Employee> employees = new ArrayList<>();
 
     public void addMember(Employee employee) {
+        employees.add(employee);
     }
 
     public List<Employee> membersAvailables(Date date) {
-        return null;
+        List<Employee> availables = new ArrayList<>();
+        for (Employee employee : employees) {
+            if(!employee.isOnHolidays(date)) {
+                availables.add(employee);
+            }
+        }
+        return availables;
     }
 }
