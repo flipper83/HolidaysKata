0e86f1616a1de8815eba0aefd6aae3ad9c67d5a4
Jorge J Barroso <jorge@karumi.com>
2015-12-29 01:05:47 +0100

    [RED] worker is on holidays.

---
+++ holidayskata/src/main/java/com/karumi/holidayskata/Employee.java
@@ -0,0 +1,15 @@
+package com.karumi.holidayskata;
+
+import java.util.Date;
+
+public class Employee {
+    private final String name;
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public boolean isOnHolidays(Date date) {
+        return false;
+    }
+}
---
+++ holidayskata/src/test/java/com/karumi/holidayskata/EmployeeTest.java
@@ -0,0 +1,30 @@
+package com.karumi.holidayskata;
+
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import static org.junit.Assert.assertTrue;
+
+public class EmployeeTest {
+    private static final String ANY_EMPLOYEE = "Paul";
+
+    @Test
+    public void shouldReturnTrueIfTheWorkerIsOnHolidaysThisDay() {
+        Employee employee = new Employee(ANY_EMPLOYEE);
+        Date employeeIsOut = givenDayEmployeeIsOut();
+
+        boolean onHolidays = employee.isOnHolidays(employeeIsOut);
+
+        assertTrue(onHolidays);
+    }
+
+    private Date givenDayEmployeeIsOut() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, 2015);
+        calendar.set(Calendar.MONTH, Calendar.JANUARY);
+        calendar.set(Calendar.DAY_OF_MONTH, 4);
+        return calendar.getTime();
+    }
+}
